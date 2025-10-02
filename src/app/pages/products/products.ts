22fb7abae62a4a43225eec3a637daaf77369c55c
leandro.filie <leandro.gfilie@gmail.com>
2025-10-01 23:49:35 -0300

    feat: create list products

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -3,10 +3,11 @@ import { ProductsService } from '../../services/products';
 import { take } from 'rxjs';
 import { IProductsResponse } from '../../interfaces/products-response';
 import { IProductResponse } from '../../interfaces/product-response';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
@@ -15,6 +16,11 @@ export class Products implements OnInit {
   private readonly _productService = inject(ProductsService);
 
   products: IProductResponse[] = [];
+  filteredProducts: IProductResponse[] = [];
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  });
 
   ngOnInit() {
     this._productService.getProducts()
@@ -22,8 +28,26 @@ export class Products implements OnInit {
     .subscribe({
       next: (response) => {
         this.products = response?.data || [];
+        this.filteredProducts = response?.data || [];
       },
     })
+  }
+
+  filterProducts() {
+    const title = this.filterForm.value?.title?.toLowerCase(); 
+    const status = this.filterForm.value?.status?.toLowerCase();
+
+    this.filteredProducts = this.products.filter((product) => {
+      (!title || product.title.toLowerCase().includes(title)) &&
+      (!status || product.status.toLowerCase().includes(status))
+    });
+  }
+
+  clearFilter() {
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('');
+
+    this.filteredProducts = this.products;
   }
 
 }
